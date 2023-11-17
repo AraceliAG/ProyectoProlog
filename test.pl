afabb06334281728c8f41f2cac407d078099cb7b
TACODEBISTECK <129352097+TACODEBISTECK@users.noreply.github.com>
2023-11-17 10:38:31 -0600

    FNAF
    yo

--- test.pl
+++ test.pl
@@ -1,4 +1,3 @@
-
 /*
 INTERFAZ GRAFICA: Esta parte del sistema experto es la que se encarga de
 interactuar con la persona comun, mostrar imagenes, botones, textos, etc.
@@ -23,7 +22,7 @@ Y LUEGO SOLO CONSULTAR TODO, AUTOMATICAMENTE SE ABRIRA LA VENTANA DEL PROGRAMA
  resource(agresivo, image, image('delfin2.jpg')).
  resource(aletargamiento, image, image('delfin2.jpg')).
  resource(aletas_retraidas, image, image('delfin2.jpg')).
- resource(equilibrio, image, image('delfin2.jpg')).
+ resource(metas, image, image('metas.jpg')).
  resource(independiente, image, image('independiente.jpg')). %SE AGREGA EL NOMBRE DEL ID EN ESTE CASO ES independiente Y DENTRO DEL PARENTESIS EL NOMBRE DE NUETRO ARCHIVO
  resource(amistad, image, image('amistad.jpg')).
  resource(hexamita, image, image('delfin2.jpg')).
@@ -31,7 +30,7 @@ Y LUEGO SOLO CONSULTAR TODO, AUTOMATICAMENTE SE ABRIRA LA VENTANA DEL PROGRAMA
  resource(ich, image, image('delfin2.jpg')).
  resource(curiosidad, image, image('curiosidad.jpg')).
  resource(venas_rojas, image, image('delfin2.jpg')).
- resource(vientre_hinchado, image, image('delfin2.jpg')).
+ resource(participacion, image, image('participacion.jpg')).
 
  mostrar_imagen(Pantalla, Imagen) :- new(Figura, figure),
                                      new(Bitmap, bitmap(resource(Imagen),@on)),
@@ -71,8 +70,8 @@ Y LUEGO SOLO CONSULTAR TODO, AUTOMATICAMENTE SE ABRIRA LA VENTANA DEL PROGRAMA
                 new(@btntratamiento,button('Detalles',
                 message(@prolog, mostrar_personaje,Personaje)
                 )),
-                send(@main, display,@boton,point(20,450)),
-                send(@main, display,@btntratamiento,point(138,450)).
+                send(@main, display,@boton,point(200,300)),
+                send(@main, display,@btntratamiento,point(200,350)).
 
 
 
@@ -158,7 +157,7 @@ interfaz_principal:-new(@main,dialog('TEST',
 /* BASE DE CONOCIMIENTOS: caracteristicas de personalidad, contiente ademas
 el identificador de imagenes de acuerdo a las repuestas
 */
-
+%TEST_ANIMALES
 conocimiento('ERES UN GATO',
 ['Prefieres trabajar en proyectos de manera independiente', 'Te sientes atraido por explorar lugares o actividades nuevas',
 'Disfrutas pasar tiempo con amigos y familiares cercanos','Te gusta participar en actividades ludicas o juegos en tu tiempo libre','Tienes una mentalidad orientada a objetivos y te sientes motivado por lograr metas']).
@@ -182,8 +181,8 @@ conocimiento('ERES UN PINWINO',
 id_imagen_preg('Prefieres trabajar en proyectos de manera independiente','independiente'). %EN EL SEGUNDO PARAMETRO O SEA inndependiente SE LLAMA EL ID QUE SE DECLARA EN EL INICIO
 id_imagen_preg('Te sientes atraido por explorar lugares o actividades nuevas','curiosidad').
 id_imagen_preg('Disfrutas pasar tiempo con amigos y familiares cercanos','amistad').
-id_imagen_preg('Te gusta participar en actividades ludicas o juegos en tu tiempo libre','vientre_hinchado').
-id_imagen_preg('Tienes una mentalidad orientada a objetivos y te sientes motivado por lograr metas','equilibrio').
+id_imagen_preg('Te gusta participar en actividades ludicas o juegos en tu tiempo libre','participacion').
+id_imagen_preg('Tienes una mentalidad orientada a objetivos y te sientes motivado por lograr metas','metas').
 %TEST_PERRO
 id_imagen_preg('Consideras que eres una persona leal y confiable en tus relaciones personales y laborales','aletargamiento').
 id_imagen_preg('Te sientes comodo interactuando con nuevas personas y disfrutas de la compania de amigos y conocidos','ich').
@@ -209,6 +208,52 @@ id_imagen_preg('Te consideras una persona dedicada y dispuesta a asumir responsa
 id_imagen_preg('Eres expresivo y comunicativo en tus relaciones con los demas o tiendes a ser mas reservado en tus expresiones','hexamita').
 id_imagen_preg('Eres capaz de adaptarte a situaciones desafiantes y mantener la resistencia en condiciones dificiles','hexamita').
 
+%TEST_FNAF
+conocimiento('ERES FOXY',
+['Prefieres abordar los desafios de frente y actuar de manera decidida incluso cuando la situación es intensa', 'Te consideras alguien que a pesar de los contratiempos puede recuperarse y seguir adelante con resiliencia y determinacion',
+'Disfrutas de la interaccion social directa siendo claro y directo en tus comunicaciones incluso en situaciones intensas','Eres bueno para adaptarte a circunstancias cambiantes y para mantenerte fuerte y en funcionamiento a pesar de desafíos constantes','Eres mas competitivo y auto-suficiente o prefieres colaborar y trabajar en conjunto']).
+conocimiento('ERES FREDDYFAZBEAR',
+['Te sientes comodo tomando la iniciativa y liderando equipos en situaciones desafiantes o complejas', 'Disfrutas de estar en el centro de la atencion o prefieres mantener un perfil mas discreto en grupos sociales',
+'Eres bueno manejando situaciones impredecibles o cambiantes manteniendo la calma y tomando decisiones rapidas','Eres colaborativo o puedes tener rivalidades competitivas','Te mantienes firme o prefieres evitar conflictos']).
+conocimiento('ERES CHICA POLLITA SEXY',
+['Te identificas como alguien con una actitud energica y positiva que tiende a contagiar alegria en su entorno', 'Disfrutas participando activamente en grupos sociales siendo dinamico/a y animado/a en tu interaccion con otras personas',
+'Consideras importante proyectar una imagen amigable y colorida en tu forma de vestir o en tu presentacion personal','Tienes rasgos o habitos que te hacen destacar ya sea por tu comportamiento predecible o por algo unico que te distingue','Eres alguien que tiende a tener relaciones especificas con ciertos individuos o que interactua de manera equitativa con todos']).
+conocimiento('ERES BONNIE',
+['Te sientes atraido por actividades creativas como la musica el arte o cualquier expresion artistica', 'Te identificas mas como alguien que aunque puede ser timido en ciertas situaciones muestra una personalidad mas activa o expresiva en contextos especificos',
+'Tienes rasgos o habilidades que te hacen destacar ya sea en tu apariencia o en alguna actividad que te apasione','Te sientes mas comodo/a interactuando con un grupo selecto de personas o te sientes a gusto socializando con una amplia variedad de individuos','Eres mas cercano/a a ciertas personas o mantienes relaciones equitativas con todos']).
+conocimiento('ERES SPRINGTRAP',
+['Eres capaz de mantener la calma y tomar decisiones racionales', 'Tienes habilidades para lidiar con situaciones inesperadas o sorpresivas manteniendo la compostura y tomando medidas rapidas y eficaces',
+'Te sientes atraido por el misterio y lo oscuro en historias películas o juegos o prefieres evitarlos por completo','Eres una persona que tiende a planificar estrategicamente en situaciones dificiles o inquietantes o prefieres actuar espontaneamente','Eres capaz de mantener la calma y tomar decisiones efectivas o te sientes abrumado/a']).
+%TEST_FOXY
+id_imagen_preg('Prefieres abordar los desafios de frente y actuar de manera decidida incluso cuando la situación es intensa','escamas_levantadas').
+id_imagen_preg('Te consideras alguien que a pesar de los contratiempos puede recuperarse y seguir adelante con resiliencia y determinacion','hexamita').
+id_imagen_preg('Disfrutas de la interaccion social directa siendo claro y directo en tus comunicaciones incluso en situaciones intensas','hexamita').
+id_imagen_preg('Eres bueno para adaptarte a circunstancias cambiantes y para mantenerte fuerte y en funcionamiento a pesar de desafíos constantes','hexamita').
+id_imagen_preg('Eres mas competitivo y auto-suficiente o prefieres colaborar y trabajar en conjunto','hexamita').
+%TEST_FREDDYFAZBEAR
+id_imagen_preg('Te sientes comodo tomando la iniciativa y liderando equipos en situaciones desafiantes o complejas','hexamita').
+id_imagen_preg('Disfrutas de estar en el centro de la atencion o prefieres mantener un perfil mas discreto en grupos sociales','hexamita').
+id_imagen_preg('Eres bueno manejando situaciones impredecibles o cambiantes manteniendo la calma y tomando decisiones rapidas','hexamita').
+id_imagen_preg('Eres colaborativo o puedes tener rivalidades competitivas','hexamita').
+id_imagen_preg('Te mantienes firme o prefieres evitar conflictos','hexamita').
+%TEST_CHICA
+id_imagen_preg('Te identificas como alguien con una actitud energica y positiva que tiende a contagiar alegria en su entorno','hexamita').
+id_imagen_preg('Disfrutas participando activamente en grupos sociales siendo dinamico/a y animado/a en tu interaccion con otras personas','hexamita').
+id_imagen_preg('Consideras importante proyectar una imagen amigable y colorida en tu forma de vestir o en tu presentacion personal','hexamita').
+id_imagen_preg('Tienes rasgos o habitos que te hacen destacar ya sea por tu comportamiento predecible o por algo unico que te distingue','hexamita').
+id_imagen_preg('Eres alguien que tiende a tener relaciones especificas con ciertos individuos o que interactua de manera equitativa con todos','hexamita').
+%TEST_BONNIE
+id_imagen_preg('Te sientes atraido por actividades creativas como la musica el arte o cualquier expresion artistica','hexamita').
+id_imagen_preg('Te identificas mas como alguien que aunque puede ser timido en ciertas situaciones muestra una personalidad mas activa o expresiva en contextos especificos','hexamita').
+id_imagen_preg('Tienes rasgos o habilidades que te hacen destacar ya sea en tu apariencia o en alguna actividad que te apasione','hexamita').
+id_imagen_preg('Te sientes mas comodo/a interactuando con un grupo selecto de personas o te sientes a gusto socializando con una amplia variedad de individuos','hexamita').
+id_imagen_preg('Eres mas cercano/a a ciertas personas o mantienes relaciones equitativas con todos','hexamita').
+%TEST_SPRINGTRAP
+id_imagen_preg('Eres capaz de mantener la calma y tomar decisiones racionales','hexamita').
+id_imagen_preg('Tienes habilidades para lidiar con situaciones inesperadas o sorpresivas manteniendo la compostura y tomando medidas rapidas y eficaces','hexamita').
+id_imagen_preg('Te sientes atraido por el misterio y lo oscuro en historias películas o juegos o prefieres evitarlos por completo','hexamita').
+id_imagen_preg('Eres una persona que tiende a planificar estrategicamente en situaciones dificiles o inquietantes o prefieres actuar espontaneamente','hexamita').
+id_imagen_preg('Eres capaz de mantener la calma y tomar decisiones efectivas o te sientes abrumado/a','hexamita').
  /* MOTOR DE INFERENCIA: Esta parte del sistema experto se encarga de
  inferir cual es el diagnostico a partir de las preguntas realizadas
  */
@@ -218,12 +263,12 @@ id_imagen_preg('Eres capaz de adaptarte a situaciones desafiantes y mantener la 
   mostrar_resultado(error):-clean_scratchpad .
 
   generar_personalidad(Personalidad):-
-                            referente_respuestas(Personalidad, ListaDeSintomas),
-                            prueba_presencia_de(Personalidad, ListaDeSintomas).
+                            referente_respuestas(Personalidad, Lista),
+                            prueba_presencia_de(Personalidad, Lista).
 
 
-referente_respuestas(Personalidad, ListaDeSintomas):-
-                            conocimiento(Personalidad, ListaDeSintomas).
+referente_respuestas(Personalidad, Lista):-
+                            conocimiento(Personalidad, Lista).
 
 
 prueba_presencia_de(Personalidad, []).
